02b5e9c174b96b41722f633b5d6861a4a10f0094
ay <ay@ay.com>
2018-11-05 09:55:15 -0500

    push to qa

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 
 AppRegistry.registerComponent('movie', () => App);
+
+// Constants
+const PORT = 8090;
+const HOST = '0.0.0.0';
